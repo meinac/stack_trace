e84dfffddc34fee0835b33ebf0128613f13cf7c0
Mehmet Emin INAC <mehmetemininac@gmail.com>
2021-01-25 06:14:44 +0100

    Write trace data into file in batches

--- lib/stack_trace/persistence.rb
+++ lib/stack_trace/persistence.rb
@@ -3,16 +3,21 @@
 module StackTrace
   class Persistence
     ROOT_PATH = File.expand_path("stack_trace")
+    WRITE_BATCH_SIZE = 1_000
 
     class << self
-      def create_tracing_directory
-        Dir.mkdir(ROOT_PATH) unless Dir.exist?(ROOT_PATH)
-      end
-
       def save(data)
         create_tracing_directory
 
-        File.open(tracing_file_path, "w") { |f| f << data.to_json }.path
+        File.open(tracing_file_path, "w") do |f|
+          StringIO.new(data.to_json).each(WRITE_BATCH_SIZE) { |d| f << d } && f.path
+        end
+      end
+
+      private
+
+      def create_tracing_directory
+        Dir.mkdir(ROOT_PATH) unless Dir.exist?(ROOT_PATH)
       end
 
       def tracing_file_path
