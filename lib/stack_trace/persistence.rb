5a00d9671fa8f4c020c3ccfa2a4b1b289dfc1e07
Mehmet Emin INAC <mehmetemininac@gmail.com>
2021-06-08 22:04:21 +0300

    Limit the responsibilities of Persistence.save method

--- lib/stack_trace/persistence.rb
+++ lib/stack_trace/persistence.rb
@@ -9,14 +9,17 @@ module StackTrace
     class << self
       def save(data, extension)
         create_tracing_directory
-        file_path = tracing_file_path(extension)
 
+        write_data(data, tracing_file_path(extension))
+      end
+
+      private
+
+      def write_data(data, file_path)
         File.open(file_path, "w") do |f|
           StringIO.new(data.to_json).each(WRITE_BATCH_SIZE) { |d| f << d } && f.path
         end
       end
-
-      private
 
       def create_tracing_directory
         Dir.mkdir(root_path) unless Dir.exist?(root_path)
