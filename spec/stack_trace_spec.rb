ebe07b6c50a9cd8f7f41b895ffa1bd894fd189e7
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-04-09 15:19:09 +0200

    Fix specs

--- spec/stack_trace_spec.rb
+++ spec/stack_trace_spec.rb
@@ -18,32 +18,32 @@ RSpec.describe StackTrace do
       {
         "spans" => [
           {
-            "defined_class" => TestClass,
+            "defined_class" => "TestClass",
             "duration" => an_instance_of(Integer),
             "method_name" => :do_something,
             "receiver" => "TestClass",
             "return_value" => "2.0",
-            "self_class" => TestClass,
+            "self_class" => "TestClass",
             "singleton" => true,
             "spans" => [
               {
                 "arguments" => { :num => "4" },
-                "defined_class" => AbstractClass,
+                "defined_class" => "AbstractClass",
                 "duration" => an_instance_of(Integer),
                 "method_name" => :abstract_method,
                 "receiver" => "TestClass",
                 "return_value" => "2.0",
-                "self_class" => TestClass,
+                "self_class" => "TestClass",
                 "singleton" => true,
                 "spans" => [
                   {
                     "arguments" => { :req => "nil" },
-                    "defined_class" => Math,
+                    "defined_class" => "Math",
                     "duration" => an_instance_of(Integer),
                     "method_name" => :sqrt,
                     "receiver" => an_instance_of(String),
                     "return_value" => "2.0",
-                    "self_class" => Math,
+                    "self_class" => "Math",
                     "singleton" => true,
                     "spans" => []
                   }
